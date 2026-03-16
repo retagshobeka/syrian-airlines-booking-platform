b803bef760eb3708fa524c5b4b83b1dc4ffd0022
retagshobeka <retagshobeka19@gmail.com>
2026-03-17 01:37:07 +0300

    add mock flights data

--- backend/backend/routes/flights.js
+++ backend/backend/routes/flights.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const flights = [
+ { airline: "Syrian Air", from: "Damascus", to: "Dubai" },
+ { airline: "Cham Wings", from: "Damascus", to: "Moscow" },
+ { airline: "Fly Baghdad", from: "Damascus", to: "Cairo" }
+];
+
 router.get("/", (req, res) => {
-  res.json({ message: "Flights route working" });
+  res.json(flights);
 });
 
 module.exports = router;
